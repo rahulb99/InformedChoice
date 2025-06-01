00af46620764e7b22ef8fc25c714ede602fae1e3
Rahul Baid <rahulbaid0110@gmail.com>
2025-05-31 19:42:17 -0500

    [v1][backend] fix: sqlalchemy dialect error

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -11,14 +11,12 @@ from .core.schemas import ProductSearchResponse, AutocompleteSuggestion, Product
 load_dotenv()
 
 # PostgreSQL configuration
-DATABASE_URL = os.getenv('DATABASE_URL')
-if not DATABASE_URL:
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = os.getenv('DB_PORT')
-    DB_NAME = os.getenv('DB_NAME')
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 engine = create_async_engine(
